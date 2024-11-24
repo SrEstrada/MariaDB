b549bebe68b17385d00da5fc2136dca7af040c04
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-23 23:17:07 -0500

    Estoy intentan bastante y no me corre mi cgi, lo raro es que si me corre de forma local, asi que no se que estoy haciendo mal

--- actor_id_5.pl
+++ actor_id_5.pl
@@ -3,27 +3,39 @@ use strict;
 use warnings;
 use DBI;
 
+# Imprimir encabezado HTTP
 print "Content-type: text/html\n\n";
 
-# Conexión a la base de datos
-my $dsn = "DBI:mysql:database=prueba;host=localhost;port=3306";
-my $user = "root";
-my $password = "";
+# Manejo de excepciones
+eval {
+    # Conexión a la base de datos
+    my $dsn = "DBI:mysql:database=prueba;host=127.0.0.1;port=3306";
+    my $user = "root";
+    my $password = "";
 
-my $dbh = DBI->connect($dsn, $user, $password, { RaiseError => 1, PrintError => 0 });
+    my $dbh = DBI->connect($dsn, $user, $password, { RaiseError => 1, PrintError => 0 });
 
-# Consulta para obtener el actor con ID 5
-my $sth = $dbh->prepare("SELECT nombre FROM actores WHERE actor_id = 5");
-$sth->execute();
+    # Consulta
+    my $sth = $dbh->prepare("SELECT nombre FROM actores WHERE actor_id = 5");
+    $sth->execute();
 
-# Imprimir resultados en HTML
-print "<html><body><h1>Actor con ID 5</h1>";
-if (my @row = $sth->fetchrow_array) {
-    print "<p>Nombre: $row[0]</p>";
-} else {
-    print "<p>No se encontró un actor con ID 5.</p>";
+    # Comprobar si hay resultados
+    my $output = "<html><body><h1>Actor con ID 5</h1>";
+    if (my @row = $sth->fetchrow_array) {
+        $output .= "<p>Nombre: $row[0]</p>";
+    } else {
+        $output .= "<p>No se encontró un actor con ID 5.</p>";
+    }
+    $output .= "</body></html>";
+
+    # Imprimir resultados
+    print $output;
+
+    $sth->finish();
+    $dbh->disconnect();
+};
+
+# Manejo de errores en tiempo de ejecución
+if ($@) {
+    print "<html><body><h1>Error en el script CGI</h1><p>$@</p></body></html>";
 }
-print "</body></html>";
-
-$sth->finish();
-$dbh->disconnect();
